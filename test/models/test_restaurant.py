52372080050dfd8137ab2de9549b417dd6633c5f
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-11-28 22:07:39 -0300

    REFACTOR: test_set_number_served.

--- test/models/test_restaurant.py
+++ test/models/test_restaurant.py
@@ -38,8 +38,22 @@ class TestRestaurant:
         resultado = restaurantTest.close_restaurant()
         assert resultado == esperado
 
-    # def test_set_number_served(self):
-    #     assert False
+    @pytest.mark.parametrize(
+        "estado_inicial, total_customers, msg_esperada",
+        [
+            (True, 50, "O restaurante Bolos Da May já atendeu 50 clientes."),
+            (True, -10, "O número de clientes deve ser um inteiro positivo."),
+            (True, "um", "O número de clientes deve ser um inteiro positivo."),
+            (False, 50, "Bolos Da May está fechado!"),
+        ],
+    )
+    def test_set_number_served(
+        self, setUp, estado_inicial, total_customers, msg_esperada
+    ):
+        restaurantTest = setUp
+        restaurantTest.open = estado_inicial
+        resultado = restaurantTest.set_number_served(total_customers)
+        assert resultado == msg_esperada
 
     # def test_increment_number_served(self):
     #     assert False
